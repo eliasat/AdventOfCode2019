1f4050287976915f3df4a38aa1fd2f9ec6fcbb57
Elias Al-Tai <elias.altai@dekiru.se>
2019-12-11 20:33:44 +0100

    day two exercise 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -7,9 +7,10 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            dayOne();
+            //DayOne();
+            DayTwo();
         }
-        private static void dayOne()
+        private static void DayOne()
         {
 
             var moduleMasses = File.ReadAllLines(@"C:\Users\Administrator\source\repos\AdventOfCode\AdventOfCode\Input\input").Select(x => int.Parse(x));
@@ -29,6 +30,47 @@ namespace AdventOfCode
             }
             sum += mass/3-2;
             return RecFuel(sum, mass/3 - 2);
+        }
+        private static void DayTwo()
+        {
+            var intCode = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2," +
+                "27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9," +
+                "55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87" +
+                ",1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115" +
+                ",2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0";
+            int[] opArray = intCode.Trim().Split(',').Select(x => int.Parse(x)).ToArray();
+            var finished = false;
+            var pc = 0;
+            while (!finished)
+            {
+                switch (opArray[pc])
+                {
+                    case 1:
+                        var addend1Address = opArray[pc + 1];
+                        var addend2Address = opArray[pc + 2];
+                        var sumAddress = opArray[pc + 3];
+                        opArray[sumAddress] = opArray[addend1Address] + opArray[addend2Address];
+                        pc += 4;
+                        break;
+                    case 2:
+                        var factor1Address = opArray[pc + 1];
+                        var factor2Address = opArray[pc + 2];
+                        var productAddress = opArray[pc + 3];
+                        opArray[productAddress] = opArray[factor1Address] * opArray[factor2Address];
+                        pc += 4;
+                        break;
+                    case 99:
+                        finished = true;
+                        break;
+                    default:
+                        throw new Exception("Unknown opcode");
+                }
+            }
+            foreach(var op in opArray)
+            {
+                Console.Write($"{op}, ");
+            }
+            Console.ReadLine();
         }
     }
 }

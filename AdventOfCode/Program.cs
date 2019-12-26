561cd9a7efd8c8f9e4d59b0c072025b612a0bb6e
Elias Al-Tai <elias.altai@dekiru.se>
2019-12-26 13:48:29 +0100

    day three

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 namespace AdventOfCode
@@ -8,7 +9,8 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             //DayOne();
-            DayTwo();
+            //DayTwo();
+            DayThree();
         }
         private static void DayOne()
         {
@@ -28,8 +30,8 @@ namespace AdventOfCode
             {
                 return sum;
             }
-            sum += mass/3-2;
-            return RecFuel(sum, mass/3 - 2);
+            sum += mass / 3 - 2;
+            return RecFuel(sum, mass / 3 - 2);
         }
         private static void DayTwo()
         {
@@ -72,7 +74,7 @@ namespace AdventOfCode
                                 throw new Exception("Unknown opcode");
                         }
                     }
-                    if(opArray[0] == 19690720)
+                    if (opArray[0] == 19690720)
                     {
                         printComputerOutput(opArray);
                         return;
@@ -89,5 +91,124 @@ namespace AdventOfCode
             Console.ReadLine();
             return;
         }
+        private static void DayThree()
+        {
+            List<Line> GetWirePath(string[] moves)
+            {
+                var prevCoor = new Coordinate(); ;
+                var coord = new Coordinate();
+                var wireCoordinates = new List<Line>();
+                foreach (var move in moves)
+                {
+                    var moveLength = int.Parse(move.Substring(1));
+                    switch (move[0])
+                    {
+                        case 'R':
+                            coord.X += moveLength;
+                            break;
+                        case 'L':
+                            coord.X -= moveLength;
+                            break;
+                        case 'U':
+                            coord.Y += moveLength;
+                            break;
+                        case 'D':
+                            coord.Y -= moveLength;
+                            break;
+                        default:
+                            throw new Exception("Unknown movetyp");
+                    }
+                    var line = new Line(prevCoor, coord);
+                    wireCoordinates.Add(line);
+                    prevCoor.X = coord.X;
+                    prevCoor.Y = coord.Y;
+                }
+                return wireCoordinates;
+            }
+            var input = File.ReadAllLines(@"C:\Users\Administrator\source\repos\AdventOfCode\AdventOfCode\Input\daythree_input");
+            var wirePathA = GetWirePath(input[0].Split(','));
+            var wirePathB = GetWirePath(input[1].Split(','));
+            List<(int x, int y, int steps)> intersections = new List<(int x, int y, int steps)>();
+            var lineAStepSum = 0;
+            foreach (var lineA in wirePathA)
+            {
+                var lineBStepSum = 0;
+                foreach (var lineB in wirePathB)
+                {
+                    var xIntersections = Range(lineA.Start.X, lineA.End.X).Intersect(Range(lineB.Start.X, lineB.End.X));
+                    var yIntersections = Range(lineA.Start.Y, lineA.End.Y).Intersect(Range(lineB.Start.Y, lineB.End.Y));
+                    if (xIntersections.Any() && yIntersections.Any())
+                    {
+                        (int x, int y, int steps) intersect = (xIntersections.First(), yIntersections.First(), lineAStepSum + lineBStepSum );
+                        if(lineA.Start.X == lineA.End.X)
+                        {
+                            intersect.steps += Math.Abs(yIntersections.First() - lineA.Start.Y);
+                            intersect.steps += Math.Abs(xIntersections.First() - lineB.Start.X);
+                        }
+                        else
+                        {
+                            intersect.steps += Math.Abs(xIntersections.First() - lineA.Start.X);
+                            intersect.steps += Math.Abs(yIntersections.First() - lineB.Start.Y);
+                        }
+                        intersections.Add(intersect);
+                    }
+                    lineBStepSum += Math.Abs(lineB.End.X - lineB.Start.X) + Math.Abs(lineB.End.Y - lineB.Start.Y);
+                }
+                lineAStepSum += Math.Abs(lineA.End.X - lineA.Start.X) + Math.Abs(lineA.End.Y - lineA.Start.Y);
+            }
+            intersections = intersections.OrderBy(coordinate => coordinate.steps).ToList();
+            foreach (var inter in intersections)
+            {
+                Console.WriteLine($"({inter.x}, {inter.y}, {inter.steps})");
+            }
+        }
+        private static IEnumerable<int> Range(int min, int max)
+        {
+            var dist = Math.Abs(max - min) + 1;
+            if (max >= min)
+            {
+                var j = Enumerable.Range(min, dist);
+                return j;
+            }
+            else
+            {
+                var a = Enumerable.Range(max, dist);
+                return a;
+            }
+        }
+        class Line
+        {
+            public Coordinate Start { get; set; } = new Coordinate();
+            public Coordinate End { get; set; } = new Coordinate();
+            public Line()
+            {
+                this.Start = new Coordinate();
+                this.End = new Coordinate();
+            }
+            public Line(Coordinate start, Coordinate end)
+            {
+                this.Start = new Coordinate(start.X, start.Y);
+                this.End = new Coordinate(end.X, end.Y);
+            }
+        }
+    }
+}
+class Coordinate
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Coordinate()
+    {
+        this.X = 0;
+        this.Y = 0;
+    }
+    public Coordinate(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+    public override string ToString()
+    {
+        return $"({this.X}, {this.Y})";
     }
 }
